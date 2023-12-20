454e89cd7a3c575f10f37c2bb38a32f2f7da856b
yonatanin <yoyocnolasco@s.sfusd.edu>
2023-12-20 12:37:24 -0800

    did not work...

--- FianlPerformanceTask/Player.gd
+++ FianlPerformanceTask/Player.gd
@@ -1,11 +1,26 @@
 extends Node2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+@export var Speed = 400
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta):
-	pass
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y -= 1
+		
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * Speed
+		
+	position += velocity * delta
+	
+	
+	func _on_body_entered(body):
+		hide()
+		enemy hit.play()
